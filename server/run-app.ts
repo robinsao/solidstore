e64df5e88ef85623f587b9e0e1c27d932c7791ef
robinsao <saosovanraksa@gmail.com>
2025-11-10 12:13:14 +1100

    fix (fixes #5): uncalled cleanup function on restart in ts-node-dev

--- server/run-app.ts
+++ server/run-app.ts
@@ -8,11 +8,20 @@ const server = app.listen(PORT, () => {
   console.log(`Express server started at port ${PORT}!`);
 });
 
-function cleanup() {
-  db.close();
-  server.close(() => {
-    console.log("Express app's cleanup finished");
+async function cleanup() {
+  console.log("Express app is cleaning up");
+  await db.close();
+  await new Promise((resolve, reject) => {
+    server.close((err) => {
+      if (err) {
+        console.error("Error during server close:", err);
+        return reject(err);
+      }
+      console.log("Express app's cleanup finished");
+      resolve(true);
+    });
   });
+  process.exit(0);
 }
 
 const sigCleanup = ["SIGINT", "SIGTERM"];
