aab3a28de7f11cfb67b777dd3a718425fe2cb4db
robinsao <saosovanraksa@gmail.com>
2026-01-10 20:18:32 +1100

    fix iss #6 (theme toggle didn't work)

--- client/src/app/(navbar)/Profile.tsx
+++ client/src/app/(navbar)/Profile.tsx
@@ -6,9 +6,11 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/Popover";
+import { useTheme } from "next-themes";
 
 export default function Profile({ children }: { children: ReactElement<any> }) {
   const [_, setIsOpen] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -17,23 +19,22 @@ export default function Profile({ children }: { children: ReactElement<any> }) {
         </button>
       </PopoverTrigger>
       <PopoverContent
-        className="w-48 bg-white shadow-md
-        border-gray-300 border rounded-xl dark:bg-gray-800 dark:text-gray-100"
+        className="w-48 bg-background shadow-md
+        border-gray-300 border rounded-xl p-0 [&>*]:py-3 [&>*]:px-5"
       >
         <form className="flex justify-between p-2">
           <label htmlFor="dark-mode-toggle">Dark Mode</label>
-          <input id="dark-mode-toggle" type="checkbox" onChange={() => {}} />
+          <input id="dark-mode-toggle" type="checkbox" onChange={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")} checked={resolvedTheme === "dark"} />
         </form>
         <Link
           href={"/settings"}
-          className="visited:text-black text-black px-2 py-2 hover:bg-gray-100 block dark:visited:text-white dark:hover:bg-gray-600"
+          className="visited:text-black px-2 py-2 hover:bg-foreground/50 block dark:visited:text-white"
         >
           Settings
         </Link>
-        <hr className="border border-l-gray-300" />
         <Link
           href={`/api/auth/signout`}
-          className="visited:text-red-600 text-red-600 px-2 py-2 hover:bg-gray-100 block dark:hover:bg-gray-600"
+          className="visited:text-red-600 text-red-600 px-2 py-2 hover:bg-red-500 hover:text-white block rounded-b-xl"
           data-test="logout-btn"
         >
           Log out
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ruda, saira } from "./fonts";
 import Image from "next/image";
 import Profile from "./(navbar)/Profile";
+import { ThemeProvider } from "next-themes";
 import { auth } from "./auth";
 
 export const metadata: Metadata = {
@@ -30,28 +31,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" id="htmlElement">
+    <html lang="en" id="htmlElement" suppressHydrationWarning>
       <body className={`${saira.className} dark:bg-gray-800`}>
-        <nav className="flex justify-between py-1 px-4 sm:px-6 md:px-12 items-center">
-          <div className="flex items-center gap-2">
-            <Image
-              src="/company-logo.svg"
-              width={24}
-              height={24}
-              alt="profile photo"
-            />
-            <span className={`${ruda.className} dark:text-white`}>
-              SolidStore
-            </span>
-          </div>
-          <Profile>
-            <ProfilePicture />
-          </Profile>
-        </nav>
-        <hr className="border-green-950 dark:border-green-400" />
-        <main id="root" className="h-svh">
-          {children}
-        </main>
+        <ThemeProvider>
+          <nav className="flex justify-between py-1 px-4 sm:px-6 md:px-12 items-center">
+            <div className="flex items-center gap-2">
+              <Image
+                src="/company-logo.svg"
+                width={24}
+                height={24}
+                alt="profile photo"
+              />
+              <span className={`${ruda.className} dark:text-white`}>
+                SolidStore
+              </span>
+            </div>
+            <Profile>
+              <ProfilePicture />
+            </Profile>
+          </nav>
+          <hr className="border-green-950 dark:border-green-400" />
+          <main id="root" className="h-svh">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
